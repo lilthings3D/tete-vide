615cdc62de08338667294c6b18a505293c7875f8
lilthings3D <lilthings.3D@outlook.com>
2026-06-23 12:41:23 +0200

    Update articles.js

--- articles.js
+++ articles.js
@@ -69,7 +69,7 @@ const ARTICLES = [
     cat: "Vivre",
     tweet: "Je sors de chez ma grand-mère. Elle dit qu'elle ne vit plus vraiment, qu'elle en a assez. Et je me retrouve sans réponse : à quoi ça sert, au juste, de vivre vieux ?",
     minutes: 2,
-    corps: `<p>e sors de chez ma grand-mère, elle vient de me dire qu'elle est fatiguée de vivre, même pas en dépression, elle en a juste assez. Elle perd peu à peu ses mots, elle a perdu toutes les personnes qu'elle connaissait, ne peut plus rien faire, même regarder la télé c'est une épreuve. Elle n'arrive presque plus à marcher ni à réfléchir. Elle en a marre, et logiquement je me demande : pourquoi vivre si vieux ?</p>
+    corps: `<p>Je sors de chez ma grand-mère, elle vient de me dire qu'elle est fatiguée de vivre, même pas en dépression, elle en a juste assez. Elle perd peu à peu ses mots, elle a perdu toutes les personnes qu'elle connaissait, ne peut plus rien faire, même regarder la télé c'est une épreuve. Elle n'arrive presque plus à marcher ni à réfléchir. Elle en a marre, et logiquement je me demande : pourquoi vivre si vieux ?</p>
     
 <p>L'idée générale veut que la vie soit précieuse, qu'il faut la préserver, l'allonger, la défendre coûte que coûte. On ne discute même plus à ce sujet, ne pas tuer c'est la norme. Mais quand la personne concernée, elle, te dit qu'elle ne vit plus — qu'est-ce qu'on préserve, exactement ? La durée ? Le nombre d'années ? J'ai du mal à comprendre à quel moment c'est la quantité de vie qu'on protège, et à quel moment c'est autre chose.</p>
 
